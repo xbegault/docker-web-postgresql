dee0df9952c53b65408774f4579bd9948594cfa7
Xavier BEGAULT <xavier.begault@3ds.com>
2017-10-28 10:14:55 +0200

    add webservices container

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -4,4 +4,6 @@ ln -s /usr/local/bin/compass /usr/bin/compass
 cd /var/www/html && php -d memory_limit=-1 bin/console assets:install --symlink --relative
 cd /var/www/html && php -d memory_limit=-1 bin/console assetic:dump
 
+composer install
+
 exec "$@"
---
+++ ws/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# /usr/local/bin/nodemon --legacy-watch api.js
+
+#nodemon -L /ws
